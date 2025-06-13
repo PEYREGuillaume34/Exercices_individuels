066644ee1832cd70c542049aa01f2a973774dac0
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-06-13 12:12:30 +0200

    Exercices

--- bonjour_javascript/script.js
+++ bonjour_javascript/script.js
@@ -1,1 +1,36 @@
-dsffzqfez
+let nom = prompt("entres ton prenom") 
+
+//EXERCICE 1
+// let message = "Bonjour"
+// console.log (message)
+
+
+//EXERCICE 2
+let firstname = "Beyonce"
+let message = `Bonjour ${nom} !`
+//console.log(message)
+document.querySelector('h1').innerText = message;
+
+// //EXERCICE 3
+// function sayHello(firstname){
+
+// //let firstname = "Beyonce"
+// let message = `Bonjour ${firstname} !`
+// return(message)
+// }
+
+// console.log(sayHello("Beyonce"))
+
+
+//EXERCICE 4
+// function sayHello(firstname,hour){
+
+// if (hour>=18){return`Bonsoir ${firstname}`}
+// else if (hour<18){return`Bonjour ${firstname}`};
+// return hour
+
+// }
+
+// console.log(sayHello(`Beyonce`, 11));
+// console.log(sayHello(`Beyonce`, 18));
+// console.log(sayHello(`Beyonce`, 17));
