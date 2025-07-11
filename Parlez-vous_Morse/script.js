bafd5a0fd48a69378a3c63ec950665ecdd83f891
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-07-11 16:30:12 +0200

    Morse

---
+++ Parlez-vous_Morse/script.js
@@ -0,0 +1,115 @@
+//1
+
+function getLatinCharacterList(text) {
+	return text.toUpperCase().split("")
+}
+console.log(getLatinCharacterList("Hello, world"))
+
+
+//2
+const latinToMorse = {
+	'A': '.-',
+	'B': '-...',
+	'C': '-.-.',
+	'D': '-..',
+	'E': '.',
+	'F': '..-.',
+	'G': '--.',
+	'H': '....',
+	'I': '..',
+	'J': '.---',
+	'K': '-.-',
+	'L': '.-..',
+	'M': '--',
+	'N': '-.',
+	'O': '---',
+	'P': '.--.',
+	'Q': '--.-',
+	'R': '.-.',
+	'S': '...',
+	'T': '-',
+	'U': '..-',
+	'V': '...-',
+	'W': '.--',
+	'X': '-..-',
+	'Y': '-.--',
+	'Z': '--..'
+}
+
+function translateLatinCharacter(caractere) {
+	return latinToMorse[caractere];
+}
+console.log(translateLatinCharacter('F'));
+
+
+//3
+
+function encode(text) {
+	let tableau1 = getLatinCharacterList(text);
+	let resultat = []
+	for (let element of tableau1){
+	let morse = translateLatinCharacter(element)
+	resultat.push(morse)
+
+	}
+	return resultat.join(" ")
+
+}
+console.log(encode("Hello"));
+
+//4
+const morseToLatin = {
+  '-': "T",
+  '--': "M",
+  '---': "O",
+  '--.': "G",
+  '--.-': "Q",
+  '--..': "Z",
+  '-.': "N",
+  '-.-': "K",
+  '-.--': "Y",
+  '-.-.': "C",
+  '-..': "D",
+  '-..-': "X",
+  '-...': "B",
+  '.': "E",
+  '.-': "A",
+  '.--': "W",
+  '.---': "J",
+  '.--.': "P",
+  '.-.': "R",
+  '.-..': "L",
+  '..': "I",
+  '..-': "U",
+  '..-.': "F",
+  '...': "S",
+  '...-': "V",
+  '....': "H"
+}
+
+function getMorseCharacterList(text){
+	return text.split(" ");
+}
+console.log(getMorseCharacterList('--','.--.'));
+
+
+function translateMorseCharacter(caractere){
+return morseToLatin[caractere];
+}
+console.log(translateMorseCharacter('-..'));
+
+
+
+function decode(text){
+let tableau3 = getMorseCharacterList(text)
+
+	let resultat = []
+	for (let element of tableau3){
+		let mot = translateMorseCharacter(element)
+		resultat.push(mot)
+	}
+	return resultat.join(" ")
+	
+}
+console.log(decode('.--.','.--'));
+
